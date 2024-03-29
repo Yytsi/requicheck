00a81d8cf900126fa664406be60356bae432a848
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-03-29 13:07:21 +0200

    vite homepage update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/rotmg-raid-requirement-checker/'
-      : '/',
+  base: process.env.NODE_ENV === 'production' ? '/requicheck/' : '/',
   plugins: [react()],
 })
